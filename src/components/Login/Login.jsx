a7f3faa0f41a9846a3f4ad8abebaf07e743eaf87
Nitin Rawat <91044967+Nitin-Rawat@users.noreply.github.com>
2024-06-16 14:34:05 +0530

    img set

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -40,7 +40,7 @@ function Login() {
 
   return (
     <Box
-      bgImage="url('./public/bg_img.jpg')"
+      bgImage="url('./bg_img.jpg')"
       bgSize="cover"
       bgPosition="center"
       bgRepeat="no-repeat"
